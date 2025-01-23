a2e3906a33bfd319a1558ffac933ebe47acc76c1
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-01-23 23:28:37 +0000

    added log routes

--- src/cntlr/jobs.js
+++ src/cntlr/jobs.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { create, list, read, update, destroy } from '../svc/jobs.js'
+import fs from 'fs'
 
 const router = Router()
 
@@ -16,6 +17,16 @@ router.get('/', (_req, res) => {
 
 router.get('/:id', (req, res) => {
   res.json(read(req.params.id))
+})
+
+router.get('/:id/logs/std', (req, res) => {
+  const job = read(req.params.id)
+  res.send(fs.existsSync(job.stdLog) ? fs.readFileSync(job.stdLog, { encoding: 'utf-8' }) : '')
+})
+
+router.get('/:id/logs/err', (req, res) => {
+  const job = read(req.params.id)
+  res.send(fs.existsSync(job.errLog) ? fs.readFileSync(job.errLog, { encoding: 'utf-8' }) : '')
 })
 
 router.put('/', (req, res) => {
