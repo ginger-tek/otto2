9adeac092f800a0ded882f8e048f181ea0b40dc4
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-01-23 22:40:22 +0000

    added more routes

--- public/assets/router.js
+++ public/assets/router.js
@@ -1,8 +1,15 @@
 const routes = [
   { path: '/', redirect: '/monitor' },
   { path: '/monitor', component: () => import('./views/monitor.js') },
-  { path: '/definitions', component: () => import('./views/definitions.js') },
-  { path: '/definitions/:id', component: () => import('./views/definition.js') },
+
+  { path: '/definitions', component: () => import('./views/definitions/list.js') },
+  { path: '/definitions/new', component: () => import('./views/definitions/create.js') },
+  { path: '/definitions/:id', component: () => import('./views/definitions/edit.js') },
+
+  { path: '/executors', component: () => import('./views/executors/list.js') },
+  { path: '/executors/new', component: () => import('./views/executors/create.js') },
+  { path: '/executors/:id', component: () => import('./views/executors/edit.js') },
+
   { path: '/history', component: () => import('./views/history.js') }
 ]
 
