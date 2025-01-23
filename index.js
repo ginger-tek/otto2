e5816cf110e8fc1ed80ecd8ebcf9e5ff588974a8
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-01-23 22:38:26 +0000

    fix dir creation, serve node_modules -> /npm for front end use

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ import history from 'connect-history-api-fallback'
 import { spawn } from 'child_process'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
-import { createLogStream, createLogger, delayMs } from './src/utils.js'
 import pkg from './package.json' with { type: 'json' }
-
-if (!fs.existsSync('data')) {
-  fs.mkdirSync('data/tmp', { recursive: true })
-  fs.mkdirSync('data/logs', { recursive: true })
-  fs.mkdirSync('data/logs/jobs', { recursive: true })
-}
-
+import { createLogStream, createLogger, delayMs } from './src/utils.js'
 import apiController from './src/cntlr/api.js'
 import { list } from './src/svc/jobs.js'
 
@@ -23,6 +16,8 @@ const app = express()
 const mainLog = createLogger(createLogStream('main.log'))
 const execLog = createLogger(createLogStream('exec.log'))
 const schdLog = createLogger(createLogStream('schd.log'))
+
+Array.from(['data/tmp', 'data/logs/jobs']).map(dir => { if (!fs.existsSync(dir)) fs.mkdirSync(dir) })
 
 mainLog.info('--------------------')
 mainLog.info(`Otto v${pkg.version}`)
@@ -63,9 +58,8 @@ mainLog.info(`Websocket service started`)
 app.use(morgan('short', { stream: createLogStream('access.log') }))
 app.use(express.json())
 app.use('/api', apiController)
+app.use('/npm', express.static('node_modules'))
 app.use(history())
 app.use(express.static('public'))
 
-http.listen(port, () => {
-  mainLog.info(`Web service started on http://localhost:${port}`)
-})
+http.listen(port, () => mainLog.info(`Web service started on http://localhost:${port}`))
